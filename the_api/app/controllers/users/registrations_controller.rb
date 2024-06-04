61671489c9250bd768cf40db40c4921fe504a6c4
Korblen <malo.bastianelli@gmail.com>
2024-06-04 13:12:50 +0200

    borefzfzvdz

--- the_api/app/controllers/users/registrations_controller.rb
+++ the_api/app/controllers/users/registrations_controller.rb
@@ -1,53 +1,38 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  def new
-     super
-   end
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # POST /resource
-   def create
-     super
-   end
-
-  # GET /resource/edit
-   def edit
-    super
-   end
+  def create
+    Rails.logger.info "Received parameters: #{params.inspect}"
+    super do |resource|
+      if resource.errors.any?
+        Rails.logger.info "User sign up failed: #{resource.errors.full_messages.join(", ")}"
+      end
+    end
+  end
 
   # PUT /resource
-   def update
-     super
-   end
-
-  # DELETE /resource
-   def destroy
-     super
-   end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  def cancel
-    super
+  def update
+    super do |resource|
+      if resource.errors.any?
+        Rails.logger.info "User update failed: #{resource.errors.full_messages.join(", ")}"
+      end
+    end
   end
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # Permit the required parameters for sign up
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # Permit the required parameters for account update
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
   end
 
   # The path used after sign up.
